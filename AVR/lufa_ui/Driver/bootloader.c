f1ba90d212eb830b492e507df2ab80dd5178bd15
laszloh <laszlo.hegedues@gmail.com>
2017-04-20 14:36:46 +0200

    commit

---
+++ AVR/lufa_ui/Driver/bootloader.c
@@ -0,0 +1,65 @@
+/*
+             LUFA Library
+     Copyright (C) Dean Camera, 2016.
+
+  dean [at] fourwalledcubicle [dot] com
+           www.lufa-lib.org
+*/
+
+/*
+  Copyright 2016  Dean Camera (dean [at] fourwalledcubicle [dot] com)
+
+  Permission to use, copy, modify, distribute, and sell this
+  software and its documentation for any purpose is hereby granted
+  without fee, provided that the above copyright notice appear in
+  all copies and that both that the copyright notice and this
+  permission notice and warranty disclaimer appear in supporting
+  documentation, and that the name of the author not be used in
+  advertising or publicity pertaining to distribution of the
+  software without specific, written prior permission.
+
+  The author disclaims all warranties with regard to this
+  software, including all implied warranties of merchantability
+  and fitness.  In no event shall the author be liable for any
+  special, indirect or consequential damages or any damages
+  whatsoever resulting from loss of use, data or profits, whether
+  in an action of contract, negligence or other tortious action,
+  arising out of or in connection with the use or performance of
+  this software.
+*/
+
+#include <avr/wdt.h>
+#include <avr/io.h>
+#include <util/delay.h>
+#include <LUFA/Common/Common.h>
+#include <LUFA/Drivers/USB/USB.h>
+
+#define MagicBootKey (*(uint16_t *)(RAMEND-1))
+
+#define MAGIC_BOOT_KEY            0x7777
+#define BOOTLOADER_START_ADDRESS  ((FLASH_SIZE_BYTES - BOOTLOADER_SEC_SIZE_BYTES) >> 1)
+
+void Bootloader_Jump_Check(void) ATTR_INIT_SECTION(3);
+void Bootloader_Jump_Check(void)
+{
+    // If the reset source was the bootloader and the key is correct, clear it and jump to the bootloader
+    if ((MCUSR & (1 << WDRF)) && (Boot_Key == MAGIC_BOOT_KEY))
+    {
+        Boot_Key = 0;
+        ((void (*)(void))BOOTLOADER_START_ADDRESS)();
+    }
+}
+
+void Jump_To_Bootloader(void)
+{
+    // If USB is used, detach from the bus and reset it
+    USB_Disable();
+    // Disable all interrupts
+    cli();
+    // Wait two seconds for the USB detachment to register on the host
+    Delay_MS(2000);
+    // Set the bootloader key to the magic value and force a reset
+    Boot_Key = MAGIC_BOOT_KEY;
+    wdt_enable(WDTO_250MS);
+    for (;;);
+}
---
+++ AVR/lufa_ui/src/config/global.h
@@ -0,0 +1,9 @@
+
+#ifndef _GLOBAL_H_
+#define _GLOBAL_H_
+
+#include "IRMPConfig.h"
+#include "AppConfig.h"
+#include "LUFAConfig.h"
+
+#endif //_GLOBAL_H_
