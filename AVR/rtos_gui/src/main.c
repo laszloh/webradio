aac8af6498f32e3515352a00462be1b18381497b
laszloh <laszlo.hegedues@gmail.com>
2017-05-04 16:55:23 +0200

    Modified config
    Added static stack for idle and timer

--- AVR/rtos_gui/src/main.c
+++ AVR/rtos_gui/src/main.c
@@ -1,121 +1,6 @@
-/*
-    FreeRTOS V9.0.1 - Copyright (C) 2017 Real Time Engineers Ltd.
-    All rights reserved
-
-    VISIT http://www.FreeRTOS.org TO ENSURE YOU ARE USING THE LATEST VERSION.
-
-    This file is part of the FreeRTOS distribution.
-
-    FreeRTOS is free software; you can redistribute it and/or modify it under
-    the terms of the GNU General Public License (version 2) as published by the
-    Free Software Foundation >>>> AND MODIFIED BY <<<< the FreeRTOS exception.
-
-    ***************************************************************************
-    >>!   NOTE: The modification to the GPL is included to allow you to     !<<
-    >>!   distribute a combined work that includes FreeRTOS without being   !<<
-    >>!   obliged to provide the source code for proprietary components     !<<
-    >>!   outside of the FreeRTOS kernel.                                   !<<
-    ***************************************************************************
-
-    FreeRTOS is distributed in the hope that it will be useful, but WITHOUT ANY
-    WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-    FOR A PARTICULAR PURPOSE.  Full license text is available on the following
-    link: http://www.freertos.org/a00114.html
-
-    ***************************************************************************
-     *                                                                       *
-     *    FreeRTOS provides completely free yet professionally developed,    *
-     *    robust, strictly quality controlled, supported, and cross          *
-     *    platform software that is more than just the market leader, it     *
-     *    is the industry's de facto standard.                               *
-     *                                                                       *
-     *    Help yourself get started quickly while simultaneously helping     *
-     *    to support the FreeRTOS project by purchasing a FreeRTOS           *
-     *    tutorial book, reference manual, or both:                          *
-     *    http://www.FreeRTOS.org/Documentation                              *
-     *                                                                       *
-    ***************************************************************************
-
-    http://www.FreeRTOS.org/FAQHelp.html - Having a problem?  Start by reading
-    the FAQ page "My application does not run, what could be wrong?".  Have you
-    defined configASSERT()?
-
-    http://www.FreeRTOS.org/support - In return for receiving this top quality
-    embedded software for free we request you assist our global community by
-    participating in the support forum.
-
-    http://www.FreeRTOS.org/training - Investing in training allows your team to
-    be as productive as possible as early as possible.  Now you can receive
-    FreeRTOS training directly from Richard Barry, CEO of Real Time Engineers
-    Ltd, and the world's leading authority on the world's leading RTOS.
-
-    http://www.FreeRTOS.org/plus - A selection of FreeRTOS ecosystem products,
-    including FreeRTOS+Trace - an indispensable productivity tool, a DOS
-    compatible FAT file system, and our tiny thread aware UDP/IP stack.
-
-    http://www.FreeRTOS.org/labs - Where new FreeRTOS products go to incubate.
-    Come and try FreeRTOS+TCP, our new open source TCP/IP stack for FreeRTOS.
-
-    http://www.OpenRTOS.com - Real Time Engineers ltd. license FreeRTOS to High
-    Integrity Systems ltd. to sell under the OpenRTOS brand.  Low cost OpenRTOS
-    licenses offer ticketed support, indemnification and commercial middleware.
-
-    http://www.SafeRTOS.com - High Integrity Systems also provide a safety
-    engineered and independently SIL3 certified version for use in safety and
-    mission critical applications that require provable dependability.
-
-    1 tab == 4 spaces!
-*/
-
-/*
- * Creates all the demo application tasks, then starts the scheduler.  The WEB
- * documentation provides more details of the demo application tasks.
+/**
  *
- * Main. c also creates a task called "Check".  This only executes every three
- * seconds but has the highest priority so is guaranteed to get processor time.
- * Its main function is to check that all the other tasks are still operational.
- * Each task that does not flash an LED maintains a unique count that is
- * incremented each time the task successfully completes its function.  Should
- * any error occur within such a task the count is permanently halted.  The
- * check task inspects the count of each task to ensure it has changed since
- * the last time the check task executed.  If all the count variables have
- * changed all the tasks are still executing error free, and the check task
- * toggles an LED.  Should any task contain an error at any time the LED toggle
- * will stop.
- *
- * The LED flash and communications test tasks do not maintain a count.
  */
-
-/*
-Changes from V1.2.0
-
-	+ Changed the baud rate for the serial test from 19200 to 57600.
-
-Changes from V1.2.3
-
-	+ The integer and comtest tasks are now used when the cooperative scheduler
-	  is being used.  Previously they were only used with the preemptive
-	  scheduler.
-
-Changes from V1.2.5
-
-	+ Set the baud rate to 38400.  This has a smaller error percentage with an
-	  8MHz clock (according to the manual).
-
-Changes from V2.0.0
-
-	+ Delay periods are now specified using variables and constants of
-	  TickType_t rather than unsigned long.
-
-Changes from V2.6.1
-
-	+ The IAR and WinAVR AVR ports are now maintained separately.
-
-Changes from V4.0.5
-
-	+ Modified to demonstrate the use of co-routines.
-
-*/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -144,13 +29,13 @@ Changes from V4.0.5
 #include "FreeRTOS.h"
 #include "task.h"
 #include "croutine.h"
+#include "semphr.h"
 
 /* Priority definitions for most tasks in the application.  Somel
    tasks just use the idle priority. */
-#define mainLED_TASK_PRIORITY			( tskIDLE_PRIORITY + 1 )
-#define mainCOM_TEST_PRIORITY			( tskIDLE_PRIORITY + 2 )
-#define mainQUEUE_POLL_PRIORITY			( tskIDLE_PRIORITY + 2 )
-#define mainCHECK_TASK_PRIORITY			( tskIDLE_PRIORITY + 3 )
+#define mainIRMP_TASK_PRIORITY			( tskIDLE_PRIORITY + 1 )
+#define mainUSB_TASK_PRIORITY			( tskIDLE_PRIORITY + 1 )
+#define mainGUI_TASK_PRIORITY			( tskIDLE_PRIORITY + 2 )
 
 /** LUFA CDC Class driver interface configuration and state information. This structure is
  *  passed to all CDC Class driver functions, so that multiple instances of the same class
@@ -182,31 +67,57 @@ USB_ClassInfo_CDC_Device_t VirtualSerial_CDC_Interface =
         },
 };
 
+SemaphoreHandle_t xCdcInterface;
+
+
 /** Standard file stream for the CDC interface when set up, so that the virtual CDC COM port can be
  *  used like any regular character stream in the C APIs.
  */
 static FILE USBSerialStream;
 
 /*
- * The idle hook is used to scheduler co-routines.
- */
-void vApplicationIdleHook( void );
-
-/*
  * Error check task
  */
-static void vLedsTask( void *pvParameters );
+static void vGuiTask( void *pvParameters );
 static void vUsbTask( void *pvParameters );
 static void vIrmpTask( void *pvParameters );
+
+/*
+ * Prototypes for the standard FreeRTOS application hook (callback) functions
+ * implemented within this file.  See http://www.freertos.org/a00016.html .
+ */
+void vApplicationMallocFailedHook( void );
+void vApplicationIdleHook( void );
+void vApplicationStackOverflowHook( TaskHandle_t pxTask, char *pcTaskName );
+void vApplicationTickHook( void );
+void vApplicationGetIdleTaskMemory( StaticTask_t **ppxIdleTaskTCBBuffer, StackType_t **ppxIdleTaskStackBuffer, uint16_t *pusIdleTaskStackSize );
+void vApplicationGetTimerTaskMemory( StaticTask_t **ppxTimerTaskTCBBuffer, StackType_t **ppxTimerTaskStackBuffer, uint16_t *pusTimerTaskStackSize );
+
+/*
+ * System death notifications
+ */
+
+#define STACK_OVERFLOW				3
+#define MALLOC_FAILED				6
+#define COULD_NOT_ALLOCATE_TASK		9
+#define COULD_NOT_ALLOCATE_MUTEX	12
+
+#define ERROR_FLASH_TIMEOUT			125
+#define COUNT_WAIT_TIMEOUT			500
+
+static void die(uint8_t count);
 
 /*-----------------------------------------------------------*/
 
 int main( void )
 {
 	/* Create the tasks defined within this file. */
-	xTaskCreate( vLedsTask, "LEDs", configMINIMAL_STACK_SIZE, NULL, mainCHECK_TASK_PRIORITY, NULL );
-	xTaskCreate( vUsbTask, "USB", configMINIMAL_STACK_SIZE, NULL, mainCHECK_TASK_PRIORITY, NULL );
-	xTaskCreate( vIrmpTask, "imrp", configMINIMAL_STACK_SIZE, NULL, mainCHECK_TASK_PRIORITY, NULL );
+	if(xTaskCreate(vGuiTask, "GUI", configMINIMAL_STACK_SIZE, NULL, mainGUI_TASK_PRIORITY, NULL) == errCOULD_NOT_ALLOCATE_REQUIRED_MEMORY)
+		die(COULD_NOT_ALLOCATE_TASK);
+	if(xTaskCreate(vUsbTask, "USB", configMINIMAL_STACK_SIZE, NULL, mainUSB_TASK_PRIORITY, NULL) == errCOULD_NOT_ALLOCATE_REQUIRED_MEMORY)
+		die(COULD_NOT_ALLOCATE_TASK);
+	if(xTaskCreate(vIrmpTask, "imrp", configMINIMAL_STACK_SIZE, NULL, mainIRMP_TASK_PRIORITY, NULL) == errCOULD_NOT_ALLOCATE_REQUIRED_MEMORY)
+		die(COULD_NOT_ALLOCATE_TASK);
 
 	/* In this port, to use preemptive scheduler define configUSE_PREEMPTION
 	as 1 in portmacro.h.  To use the cooperative scheduler define
@@ -217,43 +128,76 @@ int main( void )
 }
 /*-----------------------------------------------------------*/
 
-static void vLedsTask( void *pvParameters )
+#define FLASH_RATE (1000/portTICK_PERIOD_MS)
+
+static void vGuiTask( void *pvParameters )
 {
+	TickType_t FlashTime;
 
 	/* The parameters are not used. */
 	( void ) pvParameters;
 	
 	LEDs_Init();
+	FlashTime = xTaskGetTickCount();
 
 	/* Cycle for ever, delaying then checking all the other tasks are still
 	operating without error. */
 	for( ;; )
 	{
-		LEDs_ToggleLEDs(LEDS_LED1);
-		vTaskDelay( 500 / portTICK_PERIOD_MS );
+		if( (xTaskGetTickCount() - FlashTime) >= FLASH_RATE ) {
+			FlashTime = xTaskGetTickCount();
+			LEDs_ToggleLEDs(LEDS_LED1);
+		}
+		
+		vTaskDelay(4);
 	}
 }
 
+
+#define USB_PERIOD_MS	(5 / portTICK_PERIOD_MS)
+
 static void vUsbTask( void *pvParameters )
 {
+	TickType_t xLastWakeTime;
 
 	/* The parameters are not used. */
 	( void ) pvParameters;
 	
 	USB_Init();
 	CDC_Device_CreateStream(&VirtualSerial_CDC_Interface, &USBSerialStream);
+	xCdcInterface = xSemaphoreCreateMutex();
+	if(xCdcInterface == NULL) {
+		die(COULD_NOT_ALLOCATE_MUTEX);
+	}
 
-	/* Cycle for ever, delaying then checking all the other tasks are still
-	operating without error. */
+	xLastWakeTime = xTaskGetTickCount();
+	
 	for( ;; )
 	{
+		vTaskDelayUntil(&xLastWakeTime, USB_PERIOD_MS);
+		
+		xSemaphoreTake(xCdcInterface, portMAX_DELAY);
 		CDC_Device_USBTask(&VirtualSerial_CDC_Interface);
+		xSemaphoreGive(xCdcInterface);
+		
 		USB_USBTask();
 	}
 }
 
+
+static char *itoh (char * buf, uint8_t digits, uint16_t number)
+{
+	for (buf[digits] = 0; digits--; number >>= 4)
+	{
+		buf[digits] = "0123456789ABCDEF"[number & 0x0F];
+	}
+	return buf;
+}
+
 static void vIrmpTask( void *pvParameters )
 {
+	IRMP_DATA irmp_data;
+	char buf[5];
 
 	/* The parameters are not used. */
 	( void ) pvParameters;
@@ -264,8 +208,33 @@ static void vIrmpTask( void *pvParameters )
 	operating without error. */
 	for( ;; )
 	{
-		LEDs_ToggleLEDs(LEDS_LED2);
-		vTaskDelay( 250 / portTICK_PERIOD_MS );
+		if (irmp_get_data (&irmp_data)) {
+			xSemaphoreTake(xCdcInterface, portMAX_DELAY);
+			
+			CDC_Device_SendString_P(&VirtualSerial_CDC_Interface, PSTR("protocol: 0x"));
+			itoh(buf, 2, irmp_data.protocol);
+			CDC_Device_SendString(&VirtualSerial_CDC_Interface, buf);
+			CDC_Device_SendString_P(&VirtualSerial_CDC_Interface, PSTR("   "));
+			CDC_Device_SendString_P(&VirtualSerial_CDC_Interface, (const char*)pgm_read_word (&(irmp_protocol_names[irmp_data.protocol])));
+
+			CDC_Device_SendString_P(&VirtualSerial_CDC_Interface, PSTR("   address: 0x"));
+			itoh (buf, 4, irmp_data.address);
+			CDC_Device_SendString(&VirtualSerial_CDC_Interface, buf);
+
+			CDC_Device_SendString_P(&VirtualSerial_CDC_Interface, PSTR("   command: 0x"));
+			itoh (buf, 4, irmp_data.command);
+			CDC_Device_SendString(&VirtualSerial_CDC_Interface, buf);
+
+			CDC_Device_SendString_P(&VirtualSerial_CDC_Interface, PSTR("   flags: 0x"));
+			itoh (buf, 2, irmp_data.flags);
+			CDC_Device_SendString(&VirtualSerial_CDC_Interface, buf);
+
+			CDC_Device_SendString_P(&VirtualSerial_CDC_Interface, PSTR("\r\n"));
+			
+			xSemaphoreGive(xCdcInterface);
+		}
+
+		vTaskDelay( 100 / portTICK_PERIOD_MS );
 	}
 }
 
@@ -314,3 +283,68 @@ void vApplicationIdleHook( void )
 	vCoRoutineSchedule();
 }
 
+void vApplicationMallocFailedHook(void) 
+{
+	die(MALLOC_FAILED);
+}
+
+void vApplicationStackOverflowHook( TaskHandle_t pxTask, char *pcTaskName )
+{
+	die(STACK_OVERFLOW);
+}
+
+void vApplicationTickHook( void )
+{
+	
+}
+
+void vApplicationGetIdleTaskMemory( StaticTask_t **ppxIdleTaskTCBBuffer, StackType_t **ppxIdleTaskStackBuffer, uint16_t *pusIdleTaskStackSize )
+{
+/* The buffers used by the idle task must be static so they are persistent, and
+so exist after this function returns. */
+static StaticTask_t xIdleTaskTCB;
+static StackType_t uxIdleTaskStack[ configMINIMAL_STACK_SIZE ];
+
+	/* configUSE_STATIC_ALLOCATION is set to 1, so the application has the
+	opportunity to supply the buffers that will be used by the Idle task as its
+	stack and to hold its TCB.  If these are set to NULL then the buffers will
+	be allocated dynamically, just as if xTaskCreate() had been called. */
+	*ppxIdleTaskTCBBuffer = &xIdleTaskTCB;
+	*ppxIdleTaskStackBuffer = uxIdleTaskStack;
+	*pusIdleTaskStackSize = configMINIMAL_STACK_SIZE; /* In words.  NOT in bytes! */
+}
+
+void vApplicationGetTimerTaskMemory( StaticTask_t **ppxTimerTaskTCBBuffer, StackType_t **ppxTimerTaskStackBuffer, uint16_t *pusTimerTaskStackSize )
+{
+/* The buffers used by the Timer/Daemon task must be static so they are
+persistent, and so exist after this function returns. */
+static StaticTask_t xTimerTaskTCB;
+static StackType_t uxTimerTaskStack[ configTIMER_TASK_STACK_DEPTH ];
+
+	/* configUSE_STATIC_ALLOCATION is set to 1, so the application has the
+	opportunity to supply the buffers that will be used by the Timer/RTOS daemon
+	task as its	stack and to hold its TCB.  If these are set to NULL then the
+	buffers will be allocated dynamically, just as if xTaskCreate() had been
+	called. */
+	*ppxTimerTaskTCBBuffer = &xTimerTaskTCB;
+	*ppxTimerTaskStackBuffer = uxTimerTaskStack;
+	*pusTimerTaskStackSize = configTIMER_TASK_STACK_DEPTH; /* In words.  NOT in bytes! */
+}
+
+void die(uint8_t count)
+{
+	uint8_t i;
+	
+	LEDs_Init();
+	cli();
+	
+	while(1) {
+		for(i=0;i<count;i++) {
+			LEDs_TurnOnLEDs(LEDS_ALL_LEDS);
+			_delay_ms(ERROR_FLASH_TIMEOUT/2);
+			LEDs_TurnOffLEDs(LEDS_ALL_LEDS);
+			_delay_ms(ERROR_FLASH_TIMEOUT/2);
+		}
+		_delay_ms(COUNT_WAIT_TIMEOUT);
+	}
+}
